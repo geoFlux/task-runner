1f47302a8ce32c4e7bf827e359028a53441116cf
geoflux <blakejason5@gmail.com>
2019-11-02 15:19:02 -0400

    change type to interface for sync-models

--- src/sync-models.ts
+++ src/sync-models.ts
@@ -1,15 +1,16 @@
 import { Observable, BehaviorSubject } from "rxjs";
 import { Warning } from "./warning";
+import { CancelToken } from "./cancel-token";
 //these should probably all be interfaces
-export type Task = {
+export interface Task {
     name: string;
     sectionName: string,
     sequence: number,
     info: Observable<TaskInfo>,
-    run: (cancelToken: CancelationToken) => Promise<void>
+    run: (cancelToken: CancelToken) => Promise<void>
 }
 
-export type TaskInfo = {
+export interface TaskInfo {
     name: string,
     sectionName: string,
     enterDate: string,
@@ -28,36 +29,12 @@ export type SyncInfo = {
     tasks$: Observable<TaskInfo[]>;
     progress$: Observable<number>;
     syncTime$: Observable<string>;
-    cancelToken: CancelationToken;
+    cancelToken: CancelToken;
     finished$: Observable<SyncResult>,
 }
-export type SyncResult = {
+export interface SyncResult {
     error: any,
     cancelled: boolean,
     warnings: Warning[],
 }
-export type CancelationToken = {
-    cancel: () => void,
-    isCanceled: () => boolean,
-    invalidate: () => void,
-    isValid: () => boolean,
-    waitForCancelation: () => Promise<boolean>
-}
 
-export const cancelationToken = (): CancelationToken =>{
-    const canceled$ = new BehaviorSubject(false);
-    const isValid$ = new BehaviorSubject(true);
-    return {
-        cancel: () => canceled$.next(true),
-        isCanceled: () => canceled$.value,
-        invalidate: () => isValid$.next(false),
-        isValid: () => isValid$.value,
-        waitForCancelation: async () => {
-            return canceled$.asObservable()
-                .takeUntil(isValid$.filter(t => t == false))
-                .filter(canceled => canceled == true)
-                .take(1)
-                .toPromise();
-        }
-    }
-}
